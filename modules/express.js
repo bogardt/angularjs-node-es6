eb2f512b7505d0e62aa8acac3632a9a61066d16b
Thomas Bogard <tbogard@protegys.com>
2019-02-04 10:36:09 +0100

    replace swagger module (its work now !)

--- modules/express.js
+++ modules/express.js
@@ -1,67 +1,34 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import swaggerTools from 'swagger-tools';
 import cors from 'cors';
 import morgan from 'morgan';
 import test from '../routes/test';
 
-const swaggerDoc = require('../api/swagger.json');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../api/swagger.json');
 
 export default app => {
   /**
-   * swaggerRouter configuration
+   * Configuration of differents middlewares
    */
-  const options = {
-    controllers: './controllers',
-    useStubs: process.env.NODE_ENV === 'dev'
-  };
+  app.use(cors());
+  app.use(morgan('dev'));
+  app.use(express.static(`${__dirname}/../public`));
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
   /**
-   *  Initialize the Swagger middleware
+   * Declaring api entities here
    */
-  swaggerTools.initializeMiddleware(swaggerDoc, middleware => {
-    /**
-     *  Interpret Swagger resources and attach metadata to request
-     *  must be first in swagger-tools middleware chain
-     */
-    app.use(middleware.swaggerMetadata());
+  app.use('/api/auth', test);
 
-    /**
-     *  Validate Swagger requests
-     */
-    app.use(middleware.swaggerValidator());
-
-    /**
-     *  Route validated requests to appropriate controller
-     */
-    app.use(middleware.swaggerRouter(options));
-
-    /**
-     *  Serve the Swagger documents and Swagger UI
-     */
-    app.use(middleware.swaggerUi());
-
-    /**
-     * Configuration of differents used middlewares in app
-     */
-    app.use(cors());
-    app.use(morgan('dev'));
-    app.use(express.static(`${__dirname}/../public`));
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({ extended: true }));
-
-    /**
-     * Declaring api entities here
-     */
-    app.use('/api/auth', test);
-
-    /**
-     * Redirect on current page and delete '#' angular tag
-     */
-    app.all('*', (req, res) => {
-      const url = `${req.protocol}://${req.get('host')}/#!${req.originalUrl}`;
-      console.log(`url: ${url}`);
-      res.redirect(url);
-    });
+  /**
+   * Redirect on current page and delete '#' angular tag
+   */
+  app.all('*', (req, res) => {
+    const url = `${req.protocol}://${req.get('host')}/#!${req.originalUrl}`;
+    console.log(`url: ${url}`);
+    res.redirect(url);
   });
 };
